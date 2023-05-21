d3685608c29819267f08f06bf965d9792589cb66
PROSPERO <55129997+yurashamray@users.noreply.github.com>
2023-05-21 21:52:12 +0800

    Update index.php

--- Homework/01.Introduction/M02-U06. Linux. Система из 3-х докер-контейнеров/php/index.php
+++ Homework/01.Introduction/M02-U06. Linux. Система из 3-х докер-контейнеров/php/index.php
@@ -1,7 +1,9 @@
 <?php
 
+# Включение вывода всех ошибок
 error_reporting(E_ALL);
 
+# Установление соединения с базой данных MySQL
 $link = mysqli_connect("mysql_db_container", "root", "secret123", "mifiib");
 
 if (mysqli_connect_errno()) {
@@ -11,22 +13,27 @@ if (mysqli_connect_errno()) {
 
 $data = '';
 
+# Цикл для обработки каждой строки результата запроса
 if ($result = mysqli_query($link, "SELECT * FROM user_list")) {
     while ($row = $result->fetch_assoc()) {
         //echo $row['id'].' - '.$row['name'].' - '.$row['surname'].'<br>';
 		
+	        # Добавление данных каждой строки в переменную $data
 		$data.= "<tr><td>".$row['id']."</td><td>".$row['name']."</td><td>".$row['surname']."</td></tr>";
     }
 }
 
+# Формирование HTML-кода для вывода таблицы с данными
 $html = '
 	<style>.users { padding: 0; margin: 0 auto; border: 1px solid #000; color: #000; border-collapse: collapse; } .users th { text-align: left; } .users td { padding: 5px; font-size: 14px; }</style>
 	<h1 style="text-align: center;">The Students List (MIFIIB)</h1>
 	<table class="users" padding="0" cellspacing="0"><tr><th width="100">ID</th><th width="300">Name</th><th width="300">Surname</th></tr>'.$data.'</table>
 ';
 
+# Вывод HTML-кода на экран
 echo $html;
 
+# Вывод информации о PHP-конфигурации
 phpinfo();
 
 ?>
