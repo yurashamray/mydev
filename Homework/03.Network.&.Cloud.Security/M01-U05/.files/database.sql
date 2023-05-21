f66ba880dc6fa0c99bf8e91765cff6389af7c42e
PROSPERO <55129997+yurashamray@users.noreply.github.com>
2023-05-21 21:39:44 +0800

    Update database.sql

--- Homework/03.Network.&.Cloud.Security/M01-U05/.files/database.sql
+++ Homework/03.Network.&.Cloud.Security/M01-U05/.files/database.sql
@@ -1,17 +1,23 @@
-CREATE TABLE `user_list` (
+# Создание таблицы user_list с тремя столбцами: id, name и surname.
+CREATE TABLE `user_list` ( 
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `surname` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8; # Используется движок InnoDB и кодировка символов utf8.
 
+# Вставка трех строк данных в таблицу user_list с указанными значениями для столбцов id, name и surname.
 INSERT INTO `user_list` (`id`, `name`, `surname`) VALUES
 (1, 'Pancho', 'Villa'),
 (2, 'Salvatore', 'Giuliano'),
 (3, 'Sultana', 'Daku');
 
+# Добавление первичного ключа для столбца id в таблице user_list.
 ALTER TABLE `user_list`
   ADD PRIMARY KEY (`id`);
 
+# Изменение столбца id в таблице user_list
 ALTER TABLE `user_list`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4; # Задание начального значения для автоинкремента
+
+# Подтверждение транзакции.
 COMMIT;
